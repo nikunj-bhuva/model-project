d5d0c7702fb0e7c04d9cff3ffd525796bb8159c8
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-05-29 00:20:06 +0530

    modal  project

---
+++ index.js
@@ -0,0 +1,12 @@
+// model pro // 
+
+// let container = document.querySelector(".container");
+// console.log(container);
+// let stopBtn = document.querySelector("#stop");
+// console.log(stopBtn);
+// let timeout = setTimeout(() => {
+//     container.style = "display:block";
+// }, 1000);
+// stopBtn.addEventListener("click", () => {
+//     container.style = "display:none;"
+// })
