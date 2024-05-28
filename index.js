77bffceba8963d9d2919b38331f174afca069268
nikunj-bhuva <154654013+nikunj-bhuva@users.noreply.github.com>
2024-05-29 00:25:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,15 @@
 // console.log(container);
 // let stopBtn = document.querySelector("#stop");
 // console.log(stopBtn);
+
+// after 2mili second the code is executed //
+
 // let timeout = setTimeout(() => {
 //     container.style = "display:block";
 // }, 1000);
+
+// click stop button and model is hidden code //
+
 // stopBtn.addEventListener("click", () => {
 //     container.style = "display:none;"
 // })
